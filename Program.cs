bba52e228193ceda6694bde70a1288770f1607dd
franciny arielly <francinyalexandre07@gmail.com>
2021-09-18 14:23:03 -0300

    resolução exercicio aranha

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace aranha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" ||  ||");
+            Console.WriteLine(" \\\\()//"); 
+            Console.WriteLine("//(__)\\\\");
+            Console.WriteLine("||    ||");
+
+
+
+
+        }
+    }
+}
